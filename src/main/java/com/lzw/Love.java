3639ecdcfd0233fb3ba3b5ff7a810328357ca1a1
gitee_lzw <longzhangw88@126.com>
2021-01-08 22:34:47 +0800

    分支操作

--- src/main/java/com/lzw/Love.java
+++ src/main/java/com/lzw/Love.java
@@ -4,11 +4,10 @@ public class Love {
 
     public static void main(String[] args){
         System.out.println("I really have sex this moment");
-        System.out.println("此时此刻我好想和女人做爱");
-        System.out.println("JB好痒啊");
         System.out.println("create dev branch");
         System.out.println("create dev02 branch");
         System.out.println("create dev03 branch");
         System.out.println("create dev04 branch");
+        System.out.println("分支操作.....");
     }
 }
