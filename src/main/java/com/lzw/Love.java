86f93bb02b19a58b942c0af7c7d386f98aaea8b1
gitee_lzw <longzhangw88@126.com>
2021-01-08 20:17:16 +0800

    第2次提交

--- src/main/java/com/lzw/Love.java
+++ src/main/java/com/lzw/Love.java
@@ -4,5 +4,6 @@ public class Love {
 
     public static void main(String[] args){
         System.out.println("I really have sex this moment");
+        System.out.println("此时此刻我好想和女人做爱");
     }
 }
